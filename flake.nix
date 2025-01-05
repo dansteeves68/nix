01523ca7dae827d222f2c5260acf6c4973e34a80
dansteeves68 <dan@thesteeves.org>
2025-01-05 07:00:07 -0600

    First commit

--- flake.nix
+++ flake.nix
@@ -1,31 +1,56 @@
 {
-  description = "Dan's latest flake";
+  description = "Dan's latest dotfiles flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     inputs@{
       self,
+      home-manager,
       nix-darwin,
       nixpkgs,
+      ...
     }:
     let
       configuration =
-        { pkgs, ... }:
+        { pkgs, config, ... }:
         {
           # List packages installed in system profile. To search by name, run:
           # $ nix-env -qaP | grep wget
-          environment.systemPackages = [ pkgs.vim ];
+          environment.systemPackages = with pkgs; [ kitty vim zed-editor ];
 
           # Necessary for using flakes on this system.
           nix.settings.experimental-features = "nix-command flakes";
 
           # Enable alternative shell support in nix-darwin.
+          programs.bash.enable = true;
           programs.zsh.enable = true;
+
+          system.activationScripts.applications.text = let
+            env = pkgs.buildEnv {
+            name = "system-applications";
+            paths = config.environment.systemPackages;
+            pathsToLink = "/Applications";
+          };
+          in
+            pkgs.lib.mkForce ''
+              # Set up applications.
+              echo "setting up /Applications..." >&2
+              rm -rf /Applications/Nix\ Apps
+              mkdir -p /Applications/Nix\ Apps
+              find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+              while read -r src; do
+                app_name=$(basename "$src")
+                echo "copying $src" >&2
+                ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+              done
+            '';
 
           # Set Git commit hash for darwin-version.
           system.configurationRevision = self.rev or self.dirtyRev or null;
@@ -34,13 +59,82 @@
           # $ darwin-rebuild changelog
           system.stateVersion = 5;
 
-          # The platform the configuration will be used on.
+          users.users = {
+            dan = {
+              description = "Dan Steeves";
+              home = /Users/dan;
+              name = "dan";
+            };
+          };
+
+          # nixpkgs.config.allowBroken = true;
+          # The platform the configuration will be used on
           nixpkgs.hostPlatform = "x86_64-darwin";
+        };
+      home =
+        { pkgs, ...}:
+        {
+          home.stateVersion = "24.11";
+
+          home.packages = with pkgs; [
+            uv
+          ];
+
+          programs = {
+            home-manager.enable = true;
+
+            gh.enable = true;
+
+            git = {
+              enable = true;
+              extraConfig = {
+                push.autoSetupRemote = true;
+              };
+              ignores = [ ".DS_Store" ];
+              userEmail = "dan@thesteeves.org";
+              userName = "dansteeves68";
+            };
+
+            helix.enable = true;
+            # kitty.enable = true;
+
+            # zed-editor.enable = true;
+
+            zsh = {
+              enable = true;
+              autocd = true;
+              autosuggestion.enable = true;
+              enableCompletion = true;
+              defaultKeymap = "viins"; # emacs, vicmd, or viins
+              history = {
+                expireDuplicatesFirst = true;
+                extended = true;
+                ignoreDups = true;
+                ignorePatterns = [ ];
+                ignoreSpace = true;
+                save = 10000;
+                share = true;
+                size = 10000;
+              };
+              prezto.enable = true;
+            };
+
+          };
         };
     in
     {
       # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#simple
-      darwinConfigurations."stolen" = nix-darwin.lib.darwinSystem { modules = [ configuration ]; };
+      # $ darwin-rebuild build --flake ".#stolen"
+      darwinConfigurations."stolen" = nix-darwin.lib.darwinSystem { modules =
+        [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.backupFileExtension = "backup";
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.dan = home;
+          }
+        ]; };
     };
 }
